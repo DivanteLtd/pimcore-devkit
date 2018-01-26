990a8d94aeca6986f3936465b910e118c14ae51a
Wojciech Peisert <wojtekp@sauron.pl>
2018-01-26 20:20:33 +0100

    added DocTypeService and service for it

---
+++ src/PimcoreDevkitBundle/Service/DocTypeService.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @date        26/01/2018
+ * @author      Wojciech Peisert <wpeisert@divante.pl>
+ * @copyright   Copyright (c) 2018 DIVANTE (http://divante.pl)
+ */
+
+declare(strict_types=1);
+
+namespace PimcoreDevkitBundle\Service;
+
+use Pimcore\Config;
+use Pimcore\File;
+
+/**
+ * Class DocTypeService
+ *
+ * @package PimcoreDevkitBundle\Service
+ */
+class DocTypeService
+{
+    /**
+     * @param string $name
+     * @return null|DocType
+     */
+    public function getDocTypeByName($name) : ?DocType
+    {
+        $list = new DocType\Listing();
+        $list->setFilter(function ($row) use ($name) {
+            if ($row['name'] == $name) {
+                return true;
+            }
+            return false;
+        });
+        $list->load();
+
+        $docTypes = $list->getDocTypes();
+        if (isset($docTypes[0])) {
+            return $docTypes[0];
+        }
+    }
+}
