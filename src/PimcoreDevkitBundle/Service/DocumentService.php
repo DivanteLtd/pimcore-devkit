518982a01e5d3fc8d5a346a634af5c42858ebcff
Wojciech Peisert <wojtekp@sauron.pl>
2018-01-29 16:57:55 +0100

    add func create snippet from doctype

--- src/PimcoreDevkitBundle/Service/DocumentService.php
+++ src/PimcoreDevkitBundle/Service/DocumentService.php
@@ -11,6 +11,8 @@ namespace PimcoreDevkitBundle\Service;
 
 use Pimcore\Model\Document\Folder;
 use Pimcore\Model\Document;
+use Pimcore\Model\Document\DocType;
+use Pimcore\Model\Document\Snippet;
 
 /**
  * Class DocumentService
@@ -51,5 +53,43 @@ class DocumentService
         }
 
         return $folder;
+    }
+
+    /**
+     * Creates snippet from doctype and returns it.
+     *
+     * @param int $parentId
+     * @param string $key
+     * @param string $docTypeName
+     * @return Snippet
+     * @throws \Exception
+     */
+    public function createSnippetFromDocType(int $parentId, string $key, string $docTypeName): Snippet
+    {
+        $docTypeService = new DocTypeService();
+
+        $docType = $docTypeService->getDocTypeByName($docTypeName);
+        if (false === $docType instanceof DocType) {
+            throw new \Exception("Missing document type: '$docTypeName'");
+        }
+
+        $snippet = new Snippet();
+
+        $snippet->setParentId($parentId);
+        $snippet->setKey($key);
+        $snippet->setController($docType->getController());
+        $snippet->setAction($docType->getAction());
+        $snippet->setTemplate($docType->getTemplate());
+        $snippet->setModule($docType->getModule());
+
+        $snippet->setPublished(true);
+        $snippet->setCreationDate(time());
+        $snippet->setModificationDate(time());
+        $snippet->setUserOwner(0);
+        $snippet->setUserModification(0);
+
+        $snippet->save();
+
+        return $snippet;
     }
 }
