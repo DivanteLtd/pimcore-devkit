529c3130569228f0f574deb41e1f0e48e703780c
Wojciech Peisert <wojtekp@sauron.pl>
2018-02-05 16:21:54 +0100

    added asset ws service; catch unnecessary exceptions

--- src/PimcoreDevkitBundle/Service/InstallerService.php
+++ src/PimcoreDevkitBundle/Service/InstallerService.php
@@ -12,6 +12,7 @@ namespace PimcoreDevkitBundle\Service;
 use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\DataObject\Folder as DataObject_Folder;
 use Pimcore\Model\Document\Folder as Document_Folder;
+use Pimcore\Model\Asset\Folder as Asset_Folder;
 use Pimcore\Model\WebsiteSetting;
 
 /**
@@ -30,7 +31,12 @@ class InstallerService
      */
     public function createDataObjectFolderAndWebsiteSettings(string $wsName, int $parentId, string $key)
     {
-        $setting = WebsiteSetting::getByName($wsName);
+        $setting = null;
+        try {
+            $setting = WebsiteSetting::getByName($wsName);
+        } catch (\Exception $e) {
+            // ignore
+        }
 
         if ($setting instanceof WebsiteSetting && $setting->getData()) {
             $folder = DataObject_Folder::getById($setting->getData());
@@ -65,7 +71,12 @@ class InstallerService
      */
     public function createDocumentFolderAndWebsiteSettings(string $wsName, int $parentId, string $key)
     {
-        $setting = WebsiteSetting::getByName($wsName);
+        $setting = null;
+        try {
+            $setting = WebsiteSetting::getByName($wsName);
+        } catch (\Exception $e) {
+            // ignore
+        }
 
         if ($setting instanceof WebsiteSetting && $setting->getData()) {
             $folder = Document_Folder::getById($setting->getData());
@@ -86,6 +97,46 @@ class InstallerService
 
         if (!$folder instanceof Document_Folder) {
             throw new \Exception("Cannot get document folder $key ");
+        }
+
+        return $folder;
+    }
+
+    /**
+     * @param string $wsName
+     * @param int $parentId
+     * @param string $key
+     * @return Asset_Folder
+     * @throws \Exception
+     */
+    public function createAssetFolderAndWebsiteSettings(string $wsName, int $parentId, string $key)
+    {
+        $setting = null;
+        try {
+            $setting = WebsiteSetting::getByName($wsName);
+        } catch (\Exception $e) {
+            // ignore
+        }
+
+        if ($setting instanceof WebsiteSetting && $setting->getData()) {
+            $folder = Asset_Folder::getById($setting->getData());
+            if ($folder instanceof Asset_Folder) {
+                return $folder;
+            }
+        }
+
+        $assetService = new AssetService();
+        $folder = $assetService->getOrCreateAssetFolder($parentId, $key);
+        $this->setWebsiteSetting(
+            [
+                'name' => $wsName,
+                'type' => 'asset',
+                'data' => $folder->getId(),
+            ]
+        );
+
+        if (!$folder instanceof Asset_Folder) {
+            throw new \Exception("Cannot get asset folder $key ");
         }
 
         return $folder;
