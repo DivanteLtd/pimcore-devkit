425f0bbd6a22d091623ce3ffb260de47e14de19b
Kornel <kornel.kirsz@gmail.com>
2018-11-15 10:49:54 +0100

    Update PimcoreDevkitBundle.php

--- src/PimcoreDevkitBundle/PimcoreDevkitBundle.php
+++ src/PimcoreDevkitBundle/PimcoreDevkitBundle.php
@@ -2,8 +2,34 @@
 
 namespace PimcoreDevkitBundle;
 
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
 class PimcoreDevkitBundle extends AbstractPimcoreBundle
 {
+  use PackageVersionTrait;
+
+  /**
+   * @return string
+   */
+  public function getComposerPackageName(): string
+  {
+      return 'divanteltd/pimcoredevkit';
+  }
+
+  /**
+   * @return string
+   */
+  public function getNiceName()
+  {
+      return 'Pimcore 5 DevKit';
+  }
+
+  /**
+   * @return string
+   */
+  public function getDescription()
+  {
+      return 'This is a package that contains a set of tools commonly used in many projects';
+  }  
 }
