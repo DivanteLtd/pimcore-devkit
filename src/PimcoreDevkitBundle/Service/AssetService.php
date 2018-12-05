863f30d4a8fe7064a838c3c22922e4a768c1d5c2
Kuba Płaskonka <jplaskonka@divante.pl>
2018-12-05 11:28:21 +0100

    Additions to Assets Service

--- src/PimcoreDevkitBundle/Service/AssetService.php
+++ src/PimcoreDevkitBundle/Service/AssetService.php
@@ -2,6 +2,7 @@
 /**
  * @date        29/01/2018
  * @author      Wojciech Peisert <wpeisert@divante.pl>
+ * @author      Jakub Płaskonka <jplaskonka@divante.pl>
  * @copyright   Copyright (c) 2018 DIVANTE (http://divante.pl)
  */
 
@@ -9,8 +10,12 @@ declare(strict_types=1);
 
 namespace PimcoreDevkitBundle\Service;
 
+use GuzzleHttp\ClientInterface;
+use MongoDB\GridFS\Exception\FileNotFoundException;
+use Pimcore\Http\ClientFactory;
 use Pimcore\Model\Asset\Folder;
 use Pimcore\Model\Asset;
+use Pimcore\Tool;
 
 /**
  * Class AssetService
@@ -19,6 +24,10 @@ use Pimcore\Model\Asset;
  */
 class AssetService
 {
+
+    /** @var ClientInterface */
+    protected $httpClient;
+
     /**
      * Returns asset folder. If not existent, creates it.
      *
@@ -53,5 +62,66 @@ class AssetService
         }
 
         return $folder;
+    }
+
+    /**
+     * @param string $url
+     * @param Folder $targetFolder
+     * @param null $filename
+     * @return Asset
+     * @throws \Exception
+     */
+    public function getHttpAsset(string $url, Folder $targetFolder, $filename = null): Asset
+    {
+        if (!$this->httpClient) {
+            $this->httpClient = \Pimcore::getContainer()->get('pimcore.http_client');
+        }
+
+        $res = $this->httpClient->request('GET', $url, ['timeout' => 5]);
+        $statusCode = $res->getStatusCode();
+
+        if ($statusCode != 200) {
+            throw new FileNotFoundException();
+        }
+        $type = $res->getHeader('content-type')[0];
+
+        $fileType = array_search($type, Tool\Mime::$extensionMapping);
+        if (false === $fileType) {
+            $fileType = 'txt';
+        }
+
+        if (!$filename) {
+            $filename = md5((string) $res->getBody()) . "." . $fileType;
+        }
+
+        $asset = Asset::getByPath($targetFolder->getFullPath() . "/" . $filename);
+
+        if (!$asset) {
+            $asset = new Asset();
+            $asset->setFilename($filename);
+            $asset->setParent($targetFolder);
+            $asset->setData($res->getBody());
+            $asset->save();
+        }
+
+        return $asset;
+    }
+
+    /**
+     * @param string $key
+     * @param Folder $targetFolder
+     * @return Asset
+     */
+    public function getAsset(string $key, Folder $targetFolder): Asset
+    {
+        $asset = Asset::getByPath($targetFolder . "/" . $key);
+
+        if (!$asset) {
+            throw new \Symfony\Component\Filesystem\Exception\FileNotFoundException(
+                "Asset with path ". $targetFolder ."/". $key ." was not found"
+            );
+        }
+
+        return $asset;
     }
 }
