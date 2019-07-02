afe68903bcb1359fc14aca3247b96d992b3d3625
Bruno Ramalho <bramalho@divante.pl>
2019-07-02 16:02:57 +0200

    phpunit

---
+++ tests/PimcoreDevkitBundle/Service/ServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\PimcoreDevkitBundle\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class ServiceTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
